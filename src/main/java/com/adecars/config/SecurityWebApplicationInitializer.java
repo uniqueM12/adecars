145942f5ddec59fce04d1e938509a3297254ac13
uniqueM12 <chukwudereadindu@gmail.com>
2018-04-05 15:31:39 +0100

    C:\Users\uniqueM\github\adecars\src\main\java\com\adecars\config\SecurityWebApplicationInitializer

---
+++ src/main/java/com/adecars/config/SecurityWebApplicationInitializer.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+package com.adecars.config;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+/**
+ * Since 5 Apr 2018 | 15:00:53
+ *
+ * @author uniqueM
+ */
+public class SecurityWebApplicationInitializer extends AbstractSecurityWebApplicationInitializer {
+
+}
