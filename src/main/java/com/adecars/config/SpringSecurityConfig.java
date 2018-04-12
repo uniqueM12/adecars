ef1f7c53af26148ca9d95c78c5d378abb006133c
uniqueM12 <chukwudereadindu@gmail.com>
2018-04-12 21:31:44 +0100

    Just freshened up security and login form

--- src/main/java/com/adecars/config/SpringSecurityConfig.java
+++ src/main/java/com/adecars/config/SpringSecurityConfig.java
@@ -6,7 +6,6 @@ package com.adecars.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
@@ -24,24 +23,28 @@ import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 @EnableWebSecurity
 public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
 
-	 @Override
-	 public void configure(WebSecurity web) throws Exception {
-
-	 System.out.println("Code execution is reaching ignoring confugure()");
-	 web.ignoring().antMatchers("/resources/**");
-	 // Spring Security should completely ignore URLs starting with /resources/
-	 }
-
+	/**
+	 * This Method is used for high jacking requests so as to check it against the
+	 * set security measures.
+	 * <p>
+	 * The singular antMatchers() contain the String URLs for the home page and the
+	 * static resources which do not require any authentication.
+	 * </p>
+	 * <p>
+	 * <strong>Note: </strong>The method parameter should be used only once.
+	 * Therefore, if there is need to specify multiple URLs, the URL patterns should
+	 * be added to the only antMatchers() but they should be separated with comma(,)
+	 * separate. The comma delimited URLs will be resolved separately.
+	 * </p>
+	 */
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 
 		System.out.println("Code execution is reaching confugure()");
-		http.authorizeRequests().antMatchers("/", "/home").permitAll().anyRequest().authenticated().and().formLogin()
-				.loginPage("/login").permitAll().and().logout().permitAll();
+		http.authorizeRequests().antMatchers("/", "/home", "/css/**", "/js/**", "/fonts/**", "/images/**").permitAll()
+				.anyRequest().authenticated().and().formLogin().loginPage("/login").permitAll().and().logout()
+				.permitAll();
 		// This line allows access to static resources
-		// http.authorizeRequests().antMatchers("/resources/**", "adecars/css/**",
-		// "/adecars/css/**", "adecars/js/**",
-		// "/adecars/js/**").permitAll().anyRequest().permitAll();
 	}
 
 	@Bean
